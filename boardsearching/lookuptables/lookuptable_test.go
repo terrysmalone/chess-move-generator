c5a07e5c42c996e4c3fbd828d4b985da48374f97
Terry Malone <terrysmalone@hotmail.com>
2023-12-07 00:40:20 +0000

    Add lookup tables

---
+++ boardsearching/lookuptables/lookuptable_test.go
@@ -0,0 +1,84 @@
+package lookuptables
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// They're hardcoded values so just make sure I put them in right.
+// Since they double each time use that to verify
+func TestConsts(t *testing.T) {
+
+	assert.EqualValues(t, 1, A1)
+	assert.EqualValues(t, A1*uint64(2), B1)
+	assert.EqualValues(t, B1*uint64(2), C1)
+	assert.EqualValues(t, C1*uint64(2), D1)
+	assert.EqualValues(t, D1*uint64(2), E1)
+	assert.EqualValues(t, E1*uint64(2), F1)
+	assert.EqualValues(t, F1*uint64(2), G1)
+	assert.EqualValues(t, G1*uint64(2), H1)
+
+	assert.EqualValues(t, H1*uint64(2), A2)
+	assert.EqualValues(t, A2*uint64(2), B2)
+	assert.EqualValues(t, B2*uint64(2), C2)
+	assert.EqualValues(t, C2*uint64(2), D2)
+	assert.EqualValues(t, D2*uint64(2), E2)
+	assert.EqualValues(t, E2*uint64(2), F2)
+	assert.EqualValues(t, F2*uint64(2), G2)
+	assert.EqualValues(t, G2*uint64(2), H2)
+
+	assert.EqualValues(t, H2*uint64(2), A3)
+	assert.EqualValues(t, A3*uint64(2), B3)
+	assert.EqualValues(t, B3*uint64(2), C3)
+	assert.EqualValues(t, C3*uint64(2), D3)
+	assert.EqualValues(t, D3*uint64(2), E3)
+	assert.EqualValues(t, E3*uint64(2), F3)
+	assert.EqualValues(t, F3*uint64(2), G3)
+	assert.EqualValues(t, G3*uint64(2), H3)
+
+	assert.EqualValues(t, H3*uint64(2), A4)
+	assert.EqualValues(t, A4*uint64(2), B4)
+	assert.EqualValues(t, B4*uint64(2), C4)
+	assert.EqualValues(t, C4*uint64(2), D4)
+	assert.EqualValues(t, D4*uint64(2), E4)
+	assert.EqualValues(t, E4*uint64(2), F4)
+	assert.EqualValues(t, F4*uint64(2), G4)
+	assert.EqualValues(t, G4*uint64(2), H4)
+
+	assert.EqualValues(t, H4*uint64(2), A5)
+	assert.EqualValues(t, A5*uint64(2), B5)
+	assert.EqualValues(t, B5*uint64(2), C5)
+	assert.EqualValues(t, C5*uint64(2), D5)
+	assert.EqualValues(t, D5*uint64(2), E5)
+	assert.EqualValues(t, E5*uint64(2), F5)
+	assert.EqualValues(t, F5*uint64(2), G5)
+	assert.EqualValues(t, G5*uint64(2), H5)
+
+	assert.EqualValues(t, H5*uint64(2), A6)
+	assert.EqualValues(t, A6*uint64(2), B6)
+	assert.EqualValues(t, B6*uint64(2), C6)
+	assert.EqualValues(t, C6*uint64(2), D6)
+	assert.EqualValues(t, D6*uint64(2), E6)
+	assert.EqualValues(t, E6*uint64(2), F6)
+	assert.EqualValues(t, F6*uint64(2), G6)
+	assert.EqualValues(t, G6*uint64(2), H6)
+
+	assert.EqualValues(t, H6*uint64(2), A7)
+	assert.EqualValues(t, A7*uint64(2), B7)
+	assert.EqualValues(t, B7*uint64(2), C7)
+	assert.EqualValues(t, C7*uint64(2), D7)
+	assert.EqualValues(t, D7*uint64(2), E7)
+	assert.EqualValues(t, E7*uint64(2), F7)
+	assert.EqualValues(t, F7*uint64(2), G7)
+	assert.EqualValues(t, G7*uint64(2), H7)
+
+	assert.EqualValues(t, H7*uint64(2), A8)
+	assert.EqualValues(t, A8*uint64(2), B8)
+	assert.EqualValues(t, B8*uint64(2), C8)
+	assert.EqualValues(t, C8*uint64(2), D8)
+	assert.EqualValues(t, D8*uint64(2), E8)
+	assert.EqualValues(t, E8*uint64(2), F8)
+	assert.EqualValues(t, F8*uint64(2), G8)
+	assert.EqualValues(t, G8*uint64(2), H8)
+}
---
+++ boardsearching/lookuptables/lookuptables.go
@@ -0,0 +1,93 @@
+package lookuptables
+
+// Hardcoded since these will be referenced A LOT
+const (
+	A1 = uint64(1)
+	B1 = uint64(2)
+	C1 = uint64(4)
+	D1 = uint64(8)
+	E1 = uint64(16)
+	F1 = uint64(32)
+	G1 = uint64(64)
+	H1 = uint64(128)
+
+	A2 = uint64(256)
+	B2 = uint64(512)
+	C2 = uint64(1024)
+	D2 = uint64(2048)
+	E2 = uint64(4096)
+	F2 = uint64(8192)
+	G2 = uint64(16384)
+	H2 = uint64(32768)
+
+	A3 = uint64(65536)
+	B3 = uint64(131072)
+	C3 = uint64(262144)
+	D3 = uint64(524288)
+	E3 = uint64(1048576)
+	F3 = uint64(2097152)
+	G3 = uint64(4194304)
+	H3 = uint64(8388608)
+
+	A4 = uint64(16777216)
+	B4 = uint64(33554432)
+	C4 = uint64(67108864)
+	D4 = uint64(134217728)
+	E4 = uint64(268435456)
+	F4 = uint64(536870912)
+	G4 = uint64(1073741824)
+	H4 = uint64(2147483648)
+
+	A5 = uint64(4294967296)
+	B5 = uint64(8589934592)
+	C5 = uint64(17179869184)
+	D5 = uint64(34359738368)
+	E5 = uint64(68719476736)
+	F5 = uint64(137438953472)
+	G5 = uint64(274877906944)
+	H5 = uint64(549755813888)
+
+	A6 = uint64(1099511627776)
+	B6 = uint64(2199023255552)
+	C6 = uint64(4398046511104)
+	D6 = uint64(8796093022208)
+	E6 = uint64(17592186044416)
+	F6 = uint64(35184372088832)
+	G6 = uint64(70368744177664)
+	H6 = uint64(140737488355328)
+
+	A7 = uint64(281474976710656)
+	B7 = uint64(562949953421312)
+	C7 = uint64(1125899906842624)
+	D7 = uint64(2251799813685248)
+	E7 = uint64(4503599627370496)
+	F7 = uint64(9007199254740992)
+	G7 = uint64(18014398509481984)
+	H7 = uint64(36028797018963968)
+
+	A8 = uint64(72057594037927936)
+	B8 = uint64(144115188075855872)
+	C8 = uint64(288230376151711744)
+	D8 = uint64(576460752303423488)
+	E8 = uint64(1152921504606846976)
+	F8 = uint64(2305843009213693952)
+	G8 = uint64(4611686018427387904)
+	H8 = uint64(9223372036854775808)
+)
+
+var BitboardValueFromPosition [8][8]uint64
+
+func init() {
+	initialiseBitboardValueFromPosition()
+}
+
+func initialiseBitboardValueFromPosition() {
+	squareValue := uint64(1)
+
+	for row := 0; row < 8; row++ {
+		for column := 0; column < 8; column++ {
+			BitboardValueFromPosition[column][row] = squareValue
+			squareValue <<= 1
+		}
+	}
+}
